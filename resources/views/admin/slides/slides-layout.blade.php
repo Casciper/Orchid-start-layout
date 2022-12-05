d75a1f50c6f10f632623704901493fbcf4c3fcb2
Danil Pechenkov <pechenkov39@gmail.com>
2022-12-06 00:27:25 +0300

    smart select plugin

--- resources/views/admin/slides/slides-layout.blade.php
+++ resources/views/admin/slides/slides-layout.blade.php
@@ -1,3 +1,34 @@
+@php
+    use \App\Models\Slide;
+    $slides = Slide::query()->pluck('title', 'id');
+@endphp
+
+<fieldset class="mb-3" data-async="">
+    <div class="bg-white rounded shadow-sm p-4 py-4 d-flex flex-row justify-content-between">
+        <div class="form-group">
+            <div class="d-flex flex-column w-100">
+                <select style="width: 250px" class="js-selectize" name="item[select_value]" id="123">
+                    <option value=""
+                            @if($currentSlide === null)
+                                selected disabled hidden
+                            @endif
+                            >Выберите слайд</option>
+                        @foreach($slides as $key => $slide)
+                            @if($currentSlide !== null)
+                                @if($currentSlide->select_value === $key)
+                                    <option selected value="{{ $key }}">{{ $slide }}</option>
+                                @else
+                                    <option value="{{ $key }}">{{ $slide }}</option>
+                                @endif
+                            @else
+                                <option value="{{ $key }}">{{ $slide }}</option>
+                            @endif
+                        @endforeach
+                </select>
+            </div>
+        </div>
+    </div>
+</fieldset>
 <fieldset class="mb-3" data-async="">
     <div class="bg-white rounded shadow-sm p-4 py-4 d-flex flex-row justify-content-between">
         <div class="d-flex flex-column">
@@ -50,4 +81,21 @@
     </div>
 </fieldset>
 
+<link rel="stylesheet" href="https://snipp.ru/cdn/selectize.js/0.12.6/dist/css/selectize.default.css">
+<style type="text/css">
+    .select_wrp {
+        width: 300px;
+        margin: 0 auto;
+    }
+</style>
+
+<script src="https://snipp.ru/cdn/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://snipp.ru/cdn/microplugin.js/src/microplugin.js"></script>
+<script src="https://snipp.ru/cdn/sifter.js/sifter.min.js"></script>
+<script src="https://snipp.ru/cdn/selectize.js/0.12.6/dist/js/selectize.min.js"></script>
+<script>
+    $(document).ready(function(){
+        $('.js-selectize').selectize();
+    });
+</script>
 @include('components.admin.scripts.data')
